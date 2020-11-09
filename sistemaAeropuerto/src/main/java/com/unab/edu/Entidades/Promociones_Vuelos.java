a83e694d49a7187c1f6f67a3ea6b25dab3443695
MilitoJR <62968166+MilitoJR@users.noreply.github.com>
2020-11-09 15:15:30 -0600

    entidades

---
+++ sistemaAeropuerto/src/main/java/com/unab/edu/Entidades/Promociones_Vuelos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.Entidades;
+
+/**
+ *
+ * @author Milton
+ */
+public class Promociones_Vuelos {
+    
+    private int idPromociones_Vuelos;
+    private date FechaInicioPromo;
+    private date FechaFinalPromo;
+    private float Descuento;
+    private int idVuelo;
+    
+            
+}
